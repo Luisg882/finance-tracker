22a8e648bde8b08e234771788f7c1cd9247d9865
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-06-23 13:43:52 +0100

    configurate to heroku

--- your_savings/settings.py
+++ your_savings/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=j%q-8bwgzm+4$@#!0guf*64hqrq4rbf5j9+0lr=2o^ns1xm@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
