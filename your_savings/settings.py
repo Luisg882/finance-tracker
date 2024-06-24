4664f51b4821c103bfe588da144efd46802ef1a0
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-06-24 10:24:39 +0100

    create env.py. Create Profile and SavingPot models, link them to the admin.py of the account app

--- your_savings/settings.py
+++ your_savings/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=j%q-8bwgzm+4$@#!0guf*64hqrq4rbf5j9+0lr=2o^ns1xm@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -80,6 +84,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#   'secret': dj_database_url.parse(os.environ.get("SECRET_KEY"))
+#}
 
 
 # Password validation
