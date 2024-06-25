39240f5906367aa59eb50a9b9ffc03a872c32401
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-06-25 09:37:05 +0100

    link external database

--- your_savings/settings.py
+++ your_savings/settings.py
@@ -78,18 +78,16 @@ WSGI_APPLICATION = 'your_savings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES = {
-#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#   'secret': dj_database_url.parse(os.environ.get("SECRET_KEY"))
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 #}
 
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
